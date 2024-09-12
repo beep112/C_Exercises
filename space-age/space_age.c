41b24f063d0d382e1d1e01899ca8ad8a08bfd0f3
beep112 <jperndreca@outlook.com>
2024-09-12 17:18:15 -0400

    Started the space age problem, having issues

--- space-age/space_age.c
+++ space-age/space_age.c
@@ -1,1 +1,25 @@
 #include "space_age.h"
+
+float age(planet_t planet, int64_t seconds) {
+  double years = seconds / 31557600.0f;
+  switch (planet) {
+  case MERCURY:
+    return years * 0.2408467;
+  case VENUS:
+    return years * 0.61519726;
+  case EARTH:
+    return years;
+  case MARS:
+    return years * 1.8808158;
+  case JUPITER:
+    return years * 11.862615;
+  case SATURN:
+    return years * 29.447498;
+  case URANUS:
+    return years * 84.016846;
+  case NEPTUNE:
+    return years * 164.79132;
+  default:
+    return -1;
+  }
+}
