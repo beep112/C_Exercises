1ca9752fd2a0bc1096957288ffc34ddca0d9e2dc
beep112 <jperndreca@outlook.com>
2024-09-12 16:56:20 -0400

    Completed hamming distance question

--- hamming/hamming.c
+++ hamming/hamming.c
@@ -1,1 +1,17 @@
 #include "hamming.h"
+
+int compute(const char *lhs, const char *rhs) {
+  int lenLhs = strlen(lhs);
+  int lenRhs = strlen(rhs);
+  if (lenLhs == lenRhs) {
+    int count = 0;
+    for (int i = 0; i < lenLhs; i++) {
+      if (lhs[i] != rhs[i]) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+  return -1;
+}
